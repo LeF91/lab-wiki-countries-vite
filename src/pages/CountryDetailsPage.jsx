b5cf69523e929b016b54d9c5802eec986338200a
Fabien Lacordelle <falaco972@icloud.com>
2023-11-07 10:18:31 +0100

    lab done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,95 @@
-function CountryDetails() {}
+import { React } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Link } from "react-router-dom";
+const API_URL = "https://ih-countries-api.herokuapp.com/countries/";
 
-export default CountryDetails;
+function CountryDetailsPage() {
+  const [country, setCountry] = useState(null);
+  const [borders, setBorders] = useState([]);
+  const { countryId } = useParams();
+
+  const fetchCountry = async () => {
+    try {
+      const response = await axios.get(API_URL + countryId);
+
+      let tempBorders = [];
+      for (const border of response.data.borders) {
+        let promise = await axios.get(API_URL + border);
+        tempBorders.push(promise);
+      }
+
+      Promise.all(tempBorders).then((borderContries) => {
+        let finalBorders = [];
+        for (const bordercountry of borderContries) {
+          finalBorders.push([
+            bordercountry.data.alpha3Code,
+            bordercountry.data.name.common,
+            bordercountry.data._id,
+          ]);
+        }
+        setBorders(finalBorders);
+        setCountry(response.data);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCountry();
+  }, [countryId]);
+
+  if (!country) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      {/* Bootstrap container wrapper div */}
+      <div className="container">
+        <p style={{ fontSize: 24, fontWeight: "bold" }}> Country Details </p>
+        <img
+          style={{ width: "6rem", marginRight: "0.2rem" }}
+          src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+        />
+        <h1>{country.name.common}</h1>
+        <table className="table">
+          <thead />
+          <tbody>
+            <tr>
+              <td style={{ width: "30%" }}>Capital</td>
+              <td>{country.capital[0]}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>
+                {country.area} km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                <ul>
+                  {borders.map((border) => {
+                    return (
+                      <li
+                        style={{ listStyle: "none" }}
+                        key={crypto.randomUUID()}
+                      >
+                        <Link to={`/${border[0]}`}>{border[1]}</Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+export default CountryDetailsPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,56 @@
-function HomePage() {}
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+const API_URL = "https://ih-countries-api.herokuapp.com/countries";
+
+function HomePage() {
+  const [countries, setCountries] = useState(null);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setCountries(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  if (!countries) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div
+      className="container"
+      style={{ maxHeight: "90vh", overflow: "scroll" }}
+    >
+      <h1 style={{ fontSize: 24 }}>WikiCountries: Your Guide to the World</h1>
+
+      <div className="list-group">
+        {countries.map((country) => {
+          return (
+            <Link
+              key={country._id}
+              className="list-group-item list-group-item-action"
+              to={`/${country.alpha3Code}`}
+            >
+              <img
+                style={{ width: "1rem", marginRight: "0.2rem" }}
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+              />
+
+              {country.name.common}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default HomePage;
